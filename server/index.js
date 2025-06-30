bb18a4e94e2b1eef11fb5743af75c2de62455346
Tharindu M Patipolaarachchi <204168691+TMPatipolaarachchi@users.noreply.github.com>
2025-06-30 17:43:23 +0530

    update profile

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 4000;
 connectdb();
 
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5174', credentials: true }));
 
 
 app.use(express.json());  
